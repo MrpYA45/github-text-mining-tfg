611c5021c06e2a09a93b555f9a40195fc0826d1c
Pablo Fernández Bravo <pablofdezbravo@gmail.com>
2021-09-16 21:12:57 +0200

    Removed unused files

--- src/backend/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-( cd app/backend/gtmcore/ && ./install.sh )
-( cd app/backend/gtmapi/ && ./install.sh )
-( cd app/backend/gtmextraction/ && ./install.sh )
-( cd app/backend/gtmprocessing/ && ./install.sh )
--- src/backend/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-app/backend/gtmapi/bin/gtmapi &
-app/backend/gtmextraction/bin/gtmextraction &
-app/backend/gtmprocessing/bin/gtmprocessing
